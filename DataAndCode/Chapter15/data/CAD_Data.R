1d2970b2acc9704f4b4fb28c5dc0080d631205ad
grosner1 <grosner1@jhmi.edu>
2023-02-07 16:53:26 -0500

    Add files via upload

---
+++ DataAndCode/Chapter15/data/CAD_Data.R
@@ -0,0 +1,9 @@
+# Data for CAD example
+
+y = c(815, 115, 208, 327)
+n = sum(y)
+aSe = bSe = 1
+aSp = bSp = 1
+aPi = bPi = 1
+
+data <- list(n=n, y = y, aSe=aSe, bSe=bSe, aSp=aSp, bSp=bSp, aPi=aPi, bPi=bPi)
